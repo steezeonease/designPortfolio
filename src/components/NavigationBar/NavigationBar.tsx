831b224ed0c33c62fb6d926835810edb78c654b9
Eddie Liu <edwl@microsoft.com>
2022-01-15 21:49:28 -0800

    Add intro blurb, navigation bar

---
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,17 @@
+import styles from "./NavigationBar.module.scss";
+
+export const NavigationBar: React.FC = () => {
+  return (
+    <>
+      <div className={styles.navigationContainer}>
+        <a href="#">Work</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <a href="#">Resume</a>
+      </div>
+      <div>
+        <img src="./test.svg" alt="" />
+      </div>
+    </>
+  );
+};
